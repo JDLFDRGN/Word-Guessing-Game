be338e984ce192ec2b7912449fed60b5b8706b85
Wormhole16 <judelfederigan1@gmail.com>
2022-02-10 15:49:39 +0800

    Word Guessing Game

---
+++ GuessingGame/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace GuessingGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Lives = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Score = new System.Windows.Forms.Label();
+            this.submitAnswer = new System.Windows.Forms.TextBox();
+            this.submit = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.LightYellow;
+            this.label1.Location = new System.Drawing.Point(63, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 78);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Word Guessing Game";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Snap ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(26, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Lives:";
+            // 
+            // Lives
+            // 
+            this.Lives.AutoSize = true;
+            this.Lives.Font = new System.Drawing.Font("Snap ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lives.Location = new System.Drawing.Point(101, 160);
+            this.Lives.Name = "Lives";
+            this.Lives.Size = new System.Drawing.Size(0, 25);
+            this.Lives.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Snap ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(214, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Score:";
+            // 
+            // Score
+            // 
+            this.Score.AutoSize = true;
+            this.Score.Font = new System.Drawing.Font("Snap ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Score.Location = new System.Drawing.Point(292, 160);
+            this.Score.Name = "Score";
+            this.Score.Size = new System.Drawing.Size(0, 25);
+            this.Score.TabIndex = 4;
+            // 
+            // submitAnswer
+            // 
+            this.submitAnswer.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitAnswer.Location = new System.Drawing.Point(97, 297);
+            this.submitAnswer.Name = "submitAnswer";
+            this.submitAnswer.Size = new System.Drawing.Size(189, 29);
+            this.submitAnswer.TabIndex = 5;
+            // 
+            // submit
+            // 
+            this.submit.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submit.Location = new System.Drawing.Point(139, 354);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(111, 35);
+            this.submit.TabIndex = 6;
+            this.submit.Text = "Submit";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Snap ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.Location = new System.Drawing.Point(113, 238);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(151, 25);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Clue: Flower";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(372, 428);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.submit);
+            this.Controls.Add(this.submitAnswer);
+            this.Controls.Add(this.Score);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Lives);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "Word Guessing Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Lives;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label Score;
+        private System.Windows.Forms.TextBox submitAnswer;
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ GuessingGame/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace GuessingGame
+{
+    public partial class Form1 : Form
+    {
+        int lives = 3, score = 0, randomNumber = 0;
+        string inputAnswer = "";
+
+        ArrayList Flower;
+
+        Random random;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            random = new Random();
+
+            Flower = new ArrayList
+            {
+                "gumamela",
+                "sampaguita",
+                "sunflower",
+                "rose",
+                "dandelion"
+            };
+            Lives.Text = lives.ToString();
+            Score.Text = score.ToString();
+
+            randomNumber = random.Next(5);
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            inputAnswer = submitAnswer.Text;
+
+            submitAnswer.Text = "";
+
+            randomNumber = random.Next(5);
+    
+            if (inputAnswer == Flower[randomNumber].ToString())
+            {
+                score++;
+                Score.Text = score.ToString();
+            }
+            else
+            {
+                lives--;
+                Lives.Text = lives.ToString();      
+            }
+            if (lives == 0)
+            {
+                MessageBox.Show("Gameover\n\n" + "Final score: " + score);
+                submit.Enabled = false;
+                return;
+            }
+        }
+    }
+}
